de3e4b791143014efa652c22aa0f409147f3d7e0
Prashant Kashyap <prashantk.0001@gmail.com>
2024-07-21 22:52:09 +0530

    First commit + package setup + containsDuplicate

---
+++ src/ArraysNHashing/containsDuplicate.ts
@@ -0,0 +1,41 @@
+
+
+// O(n^2)
+function containsDuplicate1(nums: number[]): boolean{
+    for(let i=0; i< nums.length; i++){
+        for(let j=i+1; j< nums.length; j++){
+            if(nums[i] === nums[j]){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// nlog(n) using sort
+function containsDuplicate2(nums: number[]): boolean{
+    nums.sort();
+    for(let i=0; i< nums.length; i++){
+        if(nums[i] == nums[i+1]){
+            return true;
+        }
+    }
+    return false;
+}
+
+// O(n) using set
+function containsDuplicate(nums: number[]): boolean{
+    return nums.length !== new Set(nums).size;
+}
+
+//case 1
+const nums = [1,2,3,1];
+console.log("case 1 ", containsDuplicate(nums));
+
+//case 2
+const nums2 = [1,2,3,4]
+console.log("case 2 ", containsDuplicate(nums2));
+
+//case 1
+const nums3 = [1,1,1,3,3,4,3,2,4,2]
+console.log("case 1 ", containsDuplicate(nums3));
